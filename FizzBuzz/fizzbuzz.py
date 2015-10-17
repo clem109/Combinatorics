8634f6cb371bfc200cd6cf08492625a045b9b2d9
Clement Venard <cvenard@googlemail.com>
2015-10-17 17:02:11 +0800

    Ability to enter multiple inputs and produce outputs when constraints are met

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -1,7 +1,13 @@
 __author__ = 'clementvenard'
 
+# Please put the file input.txt in the same directory, once the program has run it will create the output in file named
+# output.txt
+#
+
 f = open('output.txt', 'r+')
 f.truncate()
+
+
 List = open("input.txt").read().splitlines()
 listLength = len(List)
 listPos = 0
@@ -9,37 +15,41 @@ listPos = 0
 for i in range(listLength):
     outputLines = []
     StringUnderTest = List[listPos]
+    listPos += 1
     ListUnderTest = StringUnderTest.split()
     ListUnderTest = [int(i) for i in ListUnderTest]
-    listPos += 1
     x = ListUnderTest[0]
     y = ListUnderTest[1]
     n = ListUnderTest[2]
     count = 1
 
-    for z in range(n):
+    if x > 20 or x == 0 or y > 20 or y == 0 or n > 100 or n < 21:
+        outputLines = []
+    else:
+        for z in range(n):
+            if (count % x == 0) & (count % y != 0):
+                outputLines.append("F")
+                count += 1
 
-        if (count % x == 0) & (count % y != 0):
-            outputLines.append("F")
-            count += 1
+            elif (count % y == 0) & (count % x != 0):
+                outputLines.append("B")
+                count += 1
 
-        elif (count % y == 0) & (count % x != 0):
-            outputLines.append("B")
-            count += 1
+            elif (count % y == 0) & (count % x == 0):
+                outputLines.append("FB")
+                count += 1
 
-        elif (count % x == 0) & (count % y == 0):
-            outputLines.append("FB")
-            count += 1
-        else:
-            outputLines.append(count)
-            count += 1
+            else:
+                outputLines.append(count)
+                count += 1
 
-    outputLines = ' '.join(str(e) for e in outputLines)
-    outputFile = open("output.txt", 'a')
-    outputFile.write(outputLines + '\n')
-    z = 0
+    if len(outputLines) > 0:
+        outputLines = ' '.join(str(e) for e in outputLines)
+        outputFile = open("output.txt", 'a')
+        outputFile.write(outputLines + '\n')
 
-    open("output.txt")
+
+
 
 
 
